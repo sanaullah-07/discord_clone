f8f0dbacfa016c0dc220847b85efb44a11779438
Sanaullah <sanaulah7082002@gmail.com>
2025-12-24 12:16:54 +0500

    More models coded, relationships established and aree being shown in admin panel

--- discord_clone/base/views.py
+++ discord_clone/base/views.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
 
-rooms = [
-    {'id': 1, 'name': 'Python Basics'},
-    {'id': 2, 'name': 'JAVA Basics'},
-    {'id': 3, 'name': 'C++ Basics'}
-]
+# rooms = [
+#     {'id': 1, 'name': 'Python Basics'},
+#     {'id': 2, 'name': 'JAVA Basics'},
+#     {'id': 3, 'name': 'C++ Basics'}
+# ]
 
 def home(request):
 
-     
+    rooms =  Room.objects.all()    
 
     return render(request, 'base/home.html', {'rooms' : rooms})
 
 def room(request, pk):
 
-    room_details = None
+    # room_details = None
 
-    for i in rooms:
+    # for i in rooms:
 
-        if i['id'] == int(pk):
+    #     if i['id'] == int(pk):
 
-            room_details = i
+    #         room_details = i
+
+    room_details = Room.objects.get(id=pk)
 
     context = {'room' : room_details}
 
